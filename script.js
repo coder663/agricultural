6e74e9aa147ea7c724a8a436a841749bfacca7a6
coder663 <expensivecodinglover@gmail.com>
2025-05-23 00:08:38 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["एक नेपाली भाषामा आधारित कृषि परामर्श सेवा !!!!!!!!!!!!!!!"],
+        typeSpeed: 20,
+        backSpeed: 90,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["यहाँ  सरल भाषामा दिइएको छ र परम्परागत कृषि प्रणालीका समस्याहरू पनि पूर्ण रूपमा समावेश गरिएको छ।"],
+        typeSpeed: 100,
+        backSpeed: 20,
+        loop: true
+    });
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
+
